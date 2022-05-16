eb422169a6a6eeb8504e74b224020dfc8c5ec324
Priyank Singhal <dpspri12060@gmail.com>
2022-05-16 10:18:32 +0530

    download button working

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,18 +6,25 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import Avatar from '@mui/material/Avatar';
+import Link from '@mui/material/Link';
 import { ImageDataContext } from '../context/ImageDataContext';
 import { Box } from '@mui/system';
 import DownloadIcon from '@mui/icons-material/Download';
 import { CardActions } from '@mui/material';
 import createTheme from '@mui/material/styles/createTheme';
+import axios from 'axios';
 
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+})
 
 export default function RecipeReviewCard() {
 
     const { data } = useContext(ImageDataContext)
     const result = data.results;
-    // console.log('This is from cards', result)
+    // console.log('This is from cards', result[0])
 
     return (
         <Box sx={{ display: 'flex' }}>
@@ -46,28 +53,33 @@ export default function RecipeReviewCard() {
                 </Card>
             ))} */}
 
-            <Card sx={{ maxWidth: 345 }}>
+            <Card sx={{ maxWidth: 345 }} elevation={20}>
                 <CardMedia
                     component="img"
-                    height="300"
+                    height="400"
                     image={data.results ? data.results[0].urls.full : null}
                     // image={card.urls.full}
                     alt="Paella dish"
                 />
-                <CardContent sx={{ display: 'flex', backgroundImage: data.results ? data.results[0].urls.full : null, background: 'transparent' }}>
+                <CardContent sx={{ display: 'flex', position: 'relative', background: 'linear-gradient(rgba(220,220,220,0), rgba(0,0,0,0.9))', marginTop: '-5rem', color: 'white' }}>
                     <Avatar sx={{ bgcolor: 'red', marginRight: '0.5rem' }} aria-label="recipe">
                         R
                     </Avatar>
-                    <div>
-                        <Typography variant="body2" color="text.secondary">
+                    <Box sx={{ marginBottom: '-2rem' }}>
+                        <Typography variant="body2" >
                             {data.results && data.results[0].user.first_name} {data.results && data.results[0].user.last_name}
                         </Typography>
-                        <IconButton aria-label="add to favorites" sx={{ fontSize: '1rem', marginLeft: '-0.7rem', marginTop: '-0.5rem' }}>
-                            <FavoriteIcon fontSize='small' />{data.results && data.results[0].likes}
-                        </IconButton>
-                    </div>
-                    <IconButton aria-label="share" sx={{ marginLeft: 'auto' }}>
-                        <DownloadIcon sx={{ marginLeft: 'auto' }} />
+                        <FavoriteIcon fontSize='inherit' sx={{ marginTop: '0.1rem', position: 'relative' }} /> {data.results && data.results[0].likes}
+                    </Box>
+                    {/* <a href={data.results[0].urls.full} download="img.png">some link</a> */}
+                    {/* <IconButton aria-label="share" sx={{ marginLeft: 'auto' }} theme={darkTheme} onClick={()=>fetch(data.results[0].links.download+'&force=true')}> */}
+                    <IconButton aria-label="share" sx={{ marginLeft: 'auto' }} theme={darkTheme}>
+                        <Link
+                            href={data.results ? data.results[0].links.download + '&force=true' : null}
+                            sx={{textDecoration: 'none', color: 'white'}}
+                        >
+                            <DownloadIcon />
+                        </Link>
                     </IconButton>
                 </CardContent>
             </Card>
